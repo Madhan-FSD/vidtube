11fb59fc3e233bfcf8703598b388e2e7d4945810
Madhan Kumar Gali <gmadhang9@gmail.com>
2025-11-02 16:49:46 +0530

    added healthcheck ping

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 import { app } from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import https from "https"; // add this for ping
 
 dotenv.config({
   path: "./.env",
 });
 
 const PORT = process.env.PORT || 8001;
+const HEALTHCHECK_URL = process.env.HEALTH_CHECK_PING_URL;
+
+function pingHealthcheck() {
+  https
+    .get(HEALTHCHECK_URL)
+    .on("error", (err) =>
+      console.error("Healthcheck ping failed:", err.message)
+    );
+  console.log("✅ Healthcheck ping sent at", new Date().toISOString());
+}
 
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+      console.log(`🚀 Server is running on port ${PORT}`);
+
+      pingHealthcheck();
+
+      setInterval(pingHealthcheck, 10 * 60 * 1000);
     });
   })
   .catch((err) => {
-    console.log("MongoDB connection error ", err);
+    console.error("❌ MongoDB connection error", err);
   });
